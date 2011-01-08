6e918d9f2d4e37a36fb2288e6dffd51a891f77f8
Robert Fletcher <lobatifricha@gmail.com>
2011-01-08 13:10:36 -0800

    some minor additions for conky, mp3, and libxml-ruby

--- system-setup.sh
+++ system-setup.sh
@@ -1,19 +1,20 @@
 set -e
 echo "first we'll update what we've got:"
 echo "----------------------------------"
+sudo add-apt-repository ppa:conky-companions/ppa
 sudo apt-get -y update && sudo apt-get upgrade
 sudo apt-get dist-upgrade
 #sudo gem update --system
 #sudo gem update
 echo "setting up editors/ides/build tools:"
 echo "----------------------------------"
-sudo apt-get install -y vim eclipse build-essential ruby ri rdoc python git curl mysql-server-5.0 zlib1g-dev coreutils libtool bison libxt-dev openvpn tunneldigger ruby1.8-dev sqlite3 libsqlite3-dev
+sudo apt-get install -y vim eclipse build-essential ruby ri rdoc python git curl mysql-server-5.0 zlib1g-dev coreutils libtool bison libxt-dev openvpn tunneldigger ruby1.8-dev sqlite3 libsqlite3-dev libxml2-dev
 echo "setting up media players:"
 echo "----------------------------------"
-sudo apt-get install -y vlc flashplugin-nonfree mplayer gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad
+sudo apt-get install -y vlc flashplugin-nonfree mplayer gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly-multiverse
 echo "installing tools, etc."
 echo "----------------------------------"
-sudo apt-get install -y gnome-do gimp conky chromium-browser compizconfig-settings-manager compiz-fusion-plugins-extra comix xsane wmctrl iotop unrar
+sudo apt-get install -y gnome-do gimp conky chromium-browser compizconfig-settings-manager compiz-fusion-plugins-extra comix xsane wmctrl iotop unrar conkygooglecalendar conkyrhythmbox
 
 if !  dpkg -l nautilus-dropbox > /dev/null; then
     echo 'dropbox not installed...'
