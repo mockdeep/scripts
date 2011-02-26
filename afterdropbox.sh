efb255d014f6119593ed3fb17f9bd2f0d5d2263b
Robert Fletcher <lobatifricha@gmail.com>
2011-02-26 11:44:14 -0800

    automating template linking, firefox plugins and conky startup, as well as adding mysql uninstall script

--- afterdropbox.sh
+++ afterdropbox.sh
@@ -20,8 +20,19 @@ ln ~/Dropbox/dotfiles/conkyrc3 ~/.conkyrc3
 ln ~/Dropbox/dotfiles/gitconfig ~/.gitconfig
 ln ~/Dropbox/dotfiles/vimrc ~/.vimrc
 
+if [[ $unamestr == 'Linux' ]]; then
+  ln ~/Dropbox/Templates/Document.odt ~/Templates/Document.odt
+  ln ~/Dropbox/Templates/Spreadsheet.ods ~/Templates/Spreadsheet.ods
+fi
+
 mkdir -p ~/.ssh
 ln ~/Dropbox/dotfiles/ssh/id_rsa ~/.ssh/id_rsa
 ln ~/Dropbox/dotfiles/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
 mkdir -p ~/.vim
 cp -rn ~/Dropbox/dotfiles/vim/* ~/.vim/
+
+# copy firefox extensions
+myDefault=`ls ~/.mozilla/firefox/ | grep default`
+cp ~/Dropbox/ffaddons/*.xpi ~/.mozilla/firefox/$myDefault/extensions/
+firefox http://userscripts.org/scripts/source/24430.user.js http://userscripts.org/scripts/source/9310.user.js http://userscripts.org/scripts/source/22275.user.js
+ln ~/Dropbox/conky_start.sh.desktop ~/.config/autostart/
---
+++ un_my.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# a script to completely remove MySQL from MacOSX
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm /etc/my.cnf
