287c34a34e96848764cc69f4d2d531059b89b510
Robert Fletcher <lobatifricha@gmail.com>
2011-06-20 13:34:33 -0700

    changing to soft links

--- afterdropbox.sh
+++ afterdropbox.sh
@@ -14,25 +14,29 @@ if ! grep Dropbox $bashfile; then
   echo '. ~/Dropbox/dotfiles/bashrc' >> $bashfile
 fi
 
-ln ~/Dropbox/dotfiles/conkyrc ~/.conkyrc
-ln ~/Dropbox/dotfiles/conkyrc2 ~/.conkyrc2
-ln ~/Dropbox/dotfiles/conkyrc3 ~/.conkyrc3
-ln ~/Dropbox/dotfiles/gitconfig ~/.gitconfig
-ln ~/Dropbox/dotfiles/vimrc ~/.vimrc
+ln -s ~/Dropbox/dotfiles/conkyrc ~/.conkyrc
+ln -s ~/Dropbox/dotfiles/conkyrc2 ~/.conkyrc2
+ln -s ~/Dropbox/dotfiles/conkyrc3 ~/.conkyrc3
+ln -s ~/Dropbox/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Dropbox/dotfiles/vimrc ~/.vimrc
+ln -s ~/Dropbox/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Dropbox/dotfiles/gtk-bookmarks ~/.gtk-bookmarks
+ln -s ~/Dropbox/dotfiles/irbrc ~/.irbrc
+ln -s ~/Dropbox/dotfiles/irb-save-history ~/.irb-save-history
+ln -s ~/Dropbox/dotfiles/jslintrc ~/.jslintrc
 
 if [[ $unamestr == 'Linux' ]]; then
-  ln ~/Dropbox/Templates/Document.odt ~/Templates/Document.odt
-  ln ~/Dropbox/Templates/Spreadsheet.ods ~/Templates/Spreadsheet.ods
+  ln -s ~/Dropbox/Templates/Document.odt ~/Templates/Document.odt
+  ln -s ~/Dropbox/Templates/Spreadsheet.ods ~/Templates/Spreadsheet.ods
 fi
 
-mkdir -p ~/.ssh
-ln ~/Dropbox/dotfiles/ssh/id_rsa ~/.ssh/id_rsa
-ln ~/Dropbox/dotfiles/ssh/id_rsa.pub ~/.ssh/id_rsa.pub
-mkdir -p ~/.vim
-cp -rn ~/Dropbox/dotfiles/vim/* ~/.vim/
+ln -s ~/Dropbox/dotfiles/ssh ~/.ssh
+ln -s ~/Dropbox/dotfiles/vim ~/.vim
+ln -s ~/Dropbox/Documents/bars.yml ~/bars.yml
+ln -s ~/Dropbox/Documents/rudo.yml ~/rudo.yml
 
 # copy firefox extensions
 myDefault=`ls ~/.mozilla/firefox/ | grep default`
 cp ~/Dropbox/ffaddons/*.xpi ~/.mozilla/firefox/$myDefault/extensions/
 firefox http://userscripts.org/scripts/source/24430.user.js http://userscripts.org/scripts/source/9310.user.js http://userscripts.org/scripts/source/22275.user.js
-ln ~/Dropbox/conky_start.sh.desktop ~/.config/autostart/
+ln -s ~/Dropbox/conky_start.sh.desktop ~/.config/autostart/
