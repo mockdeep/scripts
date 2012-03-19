f778859bca314896a6f677ec42d77e389951d541
Robert Fletcher <lobatifricha@gmail.com>
2012-03-18 23:49:21 -0700

    updating setup script

--- setup_scripts/system-setup.sh
+++ setup_scripts/system-setup.sh
@@ -6,17 +6,30 @@ sudo apt-get -y update && sudo apt-get upgrade
 sudo apt-get dist-upgrade
 echo "setting up editors/ides/build tools:"
 echo "----------------------------------"
-sudo apt-get install -y vim eclipse git
-#curl mysql-server-5.0 zlib1g-dev coreutils libtool bison libxt-dev \
-#sqlite3 libsqlite3-dev libxml2-dev \
-#libmagickwand-dev libmysqlclient-dev libmemcached-dbg
+sudo apt-get install -y vim eclipse git \
+curl mysql-server-5.0 zlib1g-dev coreutils libtool bison libxt-dev \
+sqlite3 libsqlite3-dev libxml2-dev \
+libmagickwand-dev libmysqlclient-dev libmemcached-dbg \
+postgresql postgresql-client libpq-dev
+sudo -u postgres createuser elements
+
+# update the end of /etc/postgresql/9.1/main/pg_hba.conf to look like this:
+# # TYPE  DATABASE    USER        CIDR-ADDRESS          METHOD
+# # "local" is for Unix domain socket connections only
+# local   all         all                               trust
+# # IPv4 local connections:
+# host    all         all         127.0.0.1/32          trust
+# # IPv6 local connections:
+# host    all         all         ::1/128               trust
+# then run `sudo /etc/init.d/postgresql restart`
+
 echo "setting up media players:"
 echo "----------------------------------"
-sudo apt-get install -y vlc flashplugin-nonfree \
+sudo apt-get install -y vlc adobe-flashplugin \
 gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-plugins-ugly \
-gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly-multiverse
+gstreamer0.10-plugins-bad
 echo "installing tools, etc."
 echo "----------------------------------"
 sudo apt-get install -y gnome-do gimp conky chromium-browser \
-compizconfig-settings-manager compiz-fusion-plugins-extra comix xsane \
+comix xsane \
 wmctrl iotop unrar conkygooglecalendar conkyrhythmbox tree
