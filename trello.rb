9ab0a8f948e9d50c1078346cadedb1e40f384ae7
Robert Fletcher <lobatifricha@gmail.com>
2014-03-19 23:32:29 -0700

    modify trello script to expand out lists into their own boards

--- trello.rb
+++ trello.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 require 'trello'
 
+# a script to expand a Trello list onto its own board
+
+board_id = <target board id here>
+source_list_id = <source list id here>
+
+
 Thread.abort_on_exception = true
 
 Trello.configure do |config|
@@ -8,27 +14,42 @@ Trello.configure do |config|
   config.member_token = ENV['TRELLO_MEMBER_TOKEN']
 end
 
-board_id = <board id here>
 lists = {}
-project_name = nil
+list_name = nil
 
-File.readlines('./asana.txt').each do |line|
-  line = line.chomp
-  if line.start_with?('[Project]')
-    project_name = line.split[1..-1].join(' ')
-    lists[project_name] ||= []
+source_list = Trello::List.find(source_list_id)
+
+source_list.cards.each do |card|
+  name = card.name
+  if name.include?(':')
+    list_name = name.split(':').first
   else
-    raise 'no project name!' unless project_name
-    lists[project_name] << line
+    list_name = 'General'
   end
+  lists[list_name] ||= []
+  lists[list_name] << name
 end
+
+list_names_to_delete = lists.keys.select { |k| lists[k].length == 1 }
+list_names_to_delete.each do |list_name|
+  lists['General'] += lists.delete(list_name)
+end
+
+lists.each do |name, tasks|
+  duplicate_tasks = tasks.select { |task| tasks.count(task) > 1 }
+  p duplicate_tasks if duplicate_tasks.any?
+end
+
+fail 'wtf!' if lists.any? { |k,v| v.uniq.size != v.size }
 
 board = Trello::Board.find(board_id)
 
 threads = []
 
+board_lists = board.lists
+
 lists.each do |name, tasks|
-  list = board.lists.detect { |list| list.name == name }
+  list = board_lists.detect { |list| list.name == name }
   list ||= Trello::List.create(name: name, board_id: board.id)
   threads << Thread.new do
     # create all tasks for a list in the same thread to preserve order
