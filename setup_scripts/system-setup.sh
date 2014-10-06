0bbe06c6c27bd8bc524713debd9afa52c5fb0709
Robert Fletcher <lobatifricha@gmail.com>
2014-10-05 17:25:01 -0700

    remove duplicate node

--- setup_scripts/system-setup.sh
+++ setup_scripts/system-setup.sh
@@ -14,7 +14,7 @@ mysql-server zlib1g-dev coreutils libtool bison libxt-dev \
 sqlite3 libsqlite3-dev libxml2-dev libreadline-dev \
 libmagickwand-dev libmysqlclient-dev libmemcached-dbg \
 postgresql postgresql-client postgresql-contrib libpq-dev libxslt1-dev \
-nodejs libqt4-dev libqtwebkit-dev libsasl2-dev libcurl4-gnutls-dev \
+libqt4-dev libqtwebkit-dev libsasl2-dev libcurl4-gnutls-dev \
 memcached build-essential imagemagick redis-server phantomjs nodejs
 # update the end of /etc/postgresql/9.1/main/pg_hba.conf to look like this:
 # # TYPE  DATABASE    USER        CIDR-ADDRESS          METHOD
